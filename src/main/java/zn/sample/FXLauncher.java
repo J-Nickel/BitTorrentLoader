b8f59f56e7f6802f671317d731609ad397cbed97
ZNickel <Qwfsk@yandex.ru>
2023-02-13 18:51:47 +0500

    Empty project with maven dependencies.

---
+++ src/main/java/zn/sample/FXLauncher.java
@@ -0,0 +1,19 @@
+package zn.sample;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class FXLauncher extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+    }
+}
---
+++ src/main/java/zn/sample/Main.java
@@ -0,0 +1,7 @@
+package zn.sample;
+
+public class Main {
+    public static void main(String[] args) {
+        FXLauncher.main(args);
+    }
+}
